c8ec6cf09814fde680f1f87574035871d2308184
pebels <pebels@gmail.com>
2013-03-01 14:03:31 +0100

    Integracion de Boost en Galeon.

---
+++ Src/Graphics/Overlay.cpp
@@ -0,0 +1,182 @@
+//---------------------------------------------------------------------------
+// Overlay.cpp
+//---------------------------------------------------------------------------
+
+/**
+@file Overlay.cpp
+
+Contiene la implementación de la clase que representa un Overlay.
+
+@see Graphics::COverlay
+
+@author Pablo Terrado
+@date Enero, 2013
+*/
+
+#include "Overlay.h"
+#include "Scene.h"
+#include "Server.h"
+
+#include "BaseSubsystems/Math.h"
+
+#include <assert.h>
+
+#include <OgreSceneNode.h>
+#include <OgreSceneManager.h>
+#include <OgreOverlay.h>
+#include <OgreTextAreaOverlayElement.h>
+#include <OgreOverlayElement.h>
+#include <OgreOverlayContainer.h>
+#include <OgreOverlayManager.h>
+
+namespace Graphics 
+{
+	COverlay::COverlay(Ogre::Overlay* overlay){
+		_overlay = overlay;
+	}
+
+	COverlay::COverlay(Ogre::OverlayElement* overlay){
+		if(std::string::npos != overlay->getName().find("Panel")){
+			_overlayContainer = static_cast<Ogre::OverlayContainer*>(overlay);
+		}
+		else{
+			_overlayText = static_cast<Ogre::TextAreaOverlayElement*>(overlay);
+		}
+	} // COverlay
+
+	COverlay::COverlay(const std::string &name, const std::string &type){
+
+		_overlay = NULL;
+		_overlayContainer = NULL;
+		_overlayText = NULL;
+
+		if(type.empty()){
+			_overlay = Graphics::CServer::getSingletonPtr()->getOverlayManager()->create(name);
+		}else{
+			if(type == "Panel")
+				_overlayContainer = static_cast<Ogre::OverlayContainer*> (Graphics::CServer::getSingletonPtr()->getOverlayManager()->createOverlayElement(type, name));
+			if(type == "TextArea")
+				_overlayText = static_cast<Ogre::TextAreaOverlayElement*>(Graphics::CServer::getSingletonPtr()->getOverlayManager()->createOverlayElement(type, name)) ;
+		}
+		_type = type;
+	} // COverlay
+
+	//------------------------------------------------------------
+
+	COverlay::~COverlay(){
+	
+	} // ~COverlay
+
+	//------------------------------------------------------------
+
+	void COverlay::setVisible(bool visible){
+		if(visible){
+			if(_overlay){ _overlay->show();}
+			if(_overlayContainer){ _overlayContainer->show();}
+			if(_overlayText){ _overlayText->show();}
+		}else{
+			if(_overlay){ _overlay->hide();}
+			if(_overlayContainer){ _overlayContainer->hide();}
+			if(_overlayText){ _overlayText->hide();}
+		}
+	} // setVisible
+
+	//------------------------------------------------------------
+
+	bool COverlay::isVisible(){
+
+		if(_overlay){return _overlay->isVisible();}
+		if(_overlayContainer){return _overlayContainer->isVisible();}
+		if(_overlayText){return _overlayText->isVisible();}
+	} // isVisible
+
+	//------------------------------------------------------------
+
+	void COverlay::add2D(COverlay* overlayContainer){
+		if(_overlay){
+			_overlay->add2D(overlayContainer->getOverlayContainer()); 	
+		}
+	} // add2D
+
+	//------------------------------------------------------------
+
+	
+	/*void COverlay::add3D(CScene* scene){
+		if(_overlay){
+			_overlay->add3D( scene->getSceneMgr()->getRootSceneNode());
+		}
+
+	} // add3D
+	*/
+
+	//------------------------------------------------------------
+
+
+	void COverlay::setPosition(float left, float top){
+		if(_overlayContainer){ _overlayContainer->setPosition(left, top);}
+		if(_overlayText){ _overlayText->setPosition(left, top);}
+	} // setPosition
+
+	//------------------------------------------------------------
+
+	
+	void COverlay::setDimensions(float width, float height){
+		if(_overlayContainer){ _overlayContainer->setDimensions(width, height);}
+		if(_overlayText){ _overlayText->setDimensions(width, height);}
+	} // setDimensions
+
+	//------------------------------------------------------------
+
+	
+	void COverlay::setMetricsMode(std::string unit){
+		Ogre::GuiMetricsMode metricsMode = Ogre::GMM_RELATIVE;
+		if(unit == "pixel")
+			metricsMode = Ogre::GMM_PIXELS;
+
+		if(_overlayContainer){ _overlayContainer->setMetricsMode(metricsMode);}
+		if(_overlayText){ _overlayText->setMetricsMode(metricsMode);}
+	} // setMetricsMode
+
+	//------------------------------------------------------------
+
+	
+	void COverlay::setMaterial(const std::string& material){
+		if(_overlayContainer){ _overlayContainer->setMaterialName(material);}
+		if(_overlayText){ _overlayText->setMaterialName(material);}
+	} // setMaterial
+
+	//------------------------------------------------------------
+
+	
+	void COverlay::setText(const std::string& text){
+		if(_overlayContainer){ _overlayContainer->setCaption(text);}
+		if(_overlayText){ _overlayText->setCaption(text);}
+	} // setText
+
+	//------------------------------------------------------------
+
+
+	void COverlay::setTextSize(const float textSize){
+		if(_overlayText){ _overlayText->setCharHeight(textSize);}
+	} // setTextSize
+
+	//------------------------------------------------------------
+
+
+	void COverlay::setFont(const std::string& font){
+		if(_overlayText){ 
+			_overlayText->setFontName(font);
+		}
+	} // setFont
+
+	//------------------------------------------------------------
+
+
+	void COverlay::addChild(COverlay* child){
+		if(_overlayContainer){ _overlayContainer->addChild(child->getOverlayText());}
+	} // addChild
+
+	//------------------------------------------------------------
+
+
+} // namespace Graphics
--- Src/Logic/Entity/Components/HudOverlay.cpp
+++ Src/Logic/Entity/Components/HudOverlay.cpp
@@ -20,14 +20,21 @@ Contiene la implementación del componente que controla el HUD mediante overlays.
 #include "Graphics/Scene.h"
 #include "Graphics/Entity.h"
 
+#include "Graphics/Server.h"
+#include "Graphics/Camera.h"
+#include "Graphics/Overlay.h"
+#include "Graphics/StaticEntity.h"
+
+/*
 #include <OgreSceneManager.h>
 #include <OgreOverlayManager.h>
 #include <OgreOverlayContainer.h>
 #include <OgreColourValue.h>
 #include <OgreOverlay.h>
 #include <OgreTextAreaOverlayElement.h>
+*/
 
-
+//Inclusion de los mensajes
 #include "Logic/Entity/Messages/Message.h"
 #include "Logic/Entity/Messages/MessageFloat.h"
 #include "Logic/Entity/Messages/MessageString.h"
@@ -47,9 +54,10 @@ namespace Logic
 		if(!IComponent::spawn(entity,map,entityInfo))
 			return false;
 
-		if(entityInfo->hasAttribute("life"))
+		//--Recogida de datos del mapa
+		if(entityInfo->hasAttribute("lifeMax"))
 		{
-			_health= entityInfo->getFloatAttribute("life");
+			_health= entityInfo->getFloatAttribute("lifeMax");
 		}
 
 
@@ -62,14 +70,131 @@ namespace Logic
 		{
 			_base = entityInfo->getIntAttribute("base");
 		}
-
+		//--Recogida de datos del mapa
 		
 		setVisibleHud(false);
-
 		
 		_playersInBase = 0;
 
 
+		_server = Graphics::CServer::getSingletonPtr();
+
+		_overlayPlay = _server->createOverlay( "_overlayPlay" );
+
+
+		int height = _server->getHeight();
+		int width = _server->getWidth();
+
+		float relativeWidth = (float)width/26;
+		float relativeHeight = (float)height/31;
+
+
+
+		///// panel DUMMY
+		Graphics::COverlay *panelDummy = _server->createOverlay("panelDummy",  "Panel" );
+		panelDummy->setMetricsMode("pixel");
+		panelDummy->setPosition(-1,-1);
+		panelDummy->setDimensions(1,1);
+
+		_textBoxArea[DUMMY] = _server->createOverlay("textAreaPanelDummy", "TextArea");
+		_textBoxArea[DUMMY]->setMetricsMode("pixel");
+		_textBoxArea[DUMMY]->setPosition(0,0);
+		_textBoxArea[DUMMY]->setDimensions(1,1);
+
+		std::stringstream sDummy;//create a stringstream
+		sDummy << "d";
+		_textBoxArea[DUMMY]->setText(sDummy.str());
+		_textBoxArea[DUMMY]->setTextSize(2.0);
+		_textBoxArea[DUMMY]->setFont("fuenteSimple");
+		//_textBoxArea[DUMMY]->setColour(Ogre::ColourValue::White);
+				
+		panelDummy->addChild(_textBoxArea[DUMMY]);
+
+		_overlayPlay->add2D( panelDummy );
+         // Add the panel to the overlay
+
+
+
+
+
+		///// panel health(para la vida del Jugador)
+		Graphics::COverlay* panelHealth = _server->createOverlay("panelHealth", "Panel");
+		panelHealth->setMetricsMode("pixel");
+		panelHealth->setPosition( 20, 20);
+		panelHealth->setDimensions( 40, 40 );
+		panelHealth->setMaterial("hudHealth");
+
+
+		_textBoxArea[HEALTH] =_server->createOverlay("textAreaPanelHealth", "TextArea");
+		_textBoxArea[HEALTH]->setMetricsMode("pixel");
+		_textBoxArea[HEALTH]->setPosition(45, 10);
+		_textBoxArea[HEALTH]->setDimensions(20, 20);
+
+		std::stringstream sHealth;//create a stringstream
+		sHealth << _health;//add number to the stream
+		_textBoxArea[HEALTH]->setText(sHealth.str());
+		_textBoxArea[HEALTH]->setTextSize(24.0);
+		_textBoxArea[HEALTH]->setFont("fuenteSimple");
+		//_textBoxArea[HEALTH]->setColour(Ogre::ColourValue::White);
+				
+		panelHealth->addChild(_textBoxArea[HEALTH]);
+
+		_overlayPlay->add2D( panelHealth );
+         // Add the panel to the overlay
+
+
+
+	///// panel healthBase(para la base del Jugador)
+		Graphics::COverlay* panelHealthBase = _server->createOverlay("panelHealthBase", "Panel");
+		panelHealthBase->setMetricsMode("pixel");
+		panelHealthBase->setPosition( 20, 80);
+		panelHealthBase->setDimensions( 40, 40 );
+		panelHealthBase->setMaterial("hudHealthBase");
+
+
+		_textBoxArea[HEALTHBASE] =_server->createOverlay("textAreaPanelHealthBase", "TextArea");
+		_textBoxArea[HEALTHBASE]->setMetricsMode("pixel");
+		_textBoxArea[HEALTHBASE]->setPosition(45, 10);
+		_textBoxArea[HEALTHBASE]->setDimensions(40, 20);
+
+		std::stringstream sHealthBase;//create a stringstream
+		sHealthBase << _healthBase;//add number to the stream
+		_textBoxArea[HEALTHBASE]->setText(sHealthBase.str());
+		_textBoxArea[HEALTHBASE]->setTextSize(24.0);
+		_textBoxArea[HEALTHBASE]->setFont("fuenteSimple");
+		//_textBoxArea[HEALTH]->setColour(Ogre::ColourValue::White);
+				
+		panelHealthBase->addChild(_textBoxArea[HEALTHBASE]);
+
+		_overlayPlay->add2D( panelHealthBase );
+         // Add the panel to the overlay
+
+
+
+
+	///// panel panelPlayers(para los Players enemigos que hay en nuestra base)
+		Graphics::COverlay* panelPlayers = _server->createOverlay("panelPlayers", "Panel");
+		panelPlayers->setMetricsMode("pixel");
+		panelPlayers->setPosition( 20, 125);
+		panelPlayers->setDimensions( 40, 40 );
+		panelPlayers->setMaterial("hudPlayers");
+
+		_textBoxArea[PLAYERS] =_server->createOverlay("textAreaPanelPlayers", "TextArea");
+		_textBoxArea[PLAYERS]->setMetricsMode("pixel");
+		_textBoxArea[PLAYERS]->setPosition(45, 10);
+		_textBoxArea[PLAYERS]->setDimensions(40, 20);
+
+		std::stringstream sPlayers;//create a stringstream
+		sPlayers << _healthBase;//add number to the stream
+		_textBoxArea[PLAYERS]->setText(sPlayers.str());
+		_textBoxArea[PLAYERS]->setTextSize(24.0);
+		_textBoxArea[PLAYERS]->setFont("fuenteSimple");
+		//_textBoxArea[HEALTH]->setColour(Ogre::ColourValue::White);
+				
+		panelPlayers->addChild(_textBoxArea[PLAYERS]);
+
+		_overlayPlay->add2D( panelPlayers );
+         // Add the panel to the overlay
 
 
 		return true;
@@ -80,6 +205,7 @@ namespace Logic
 	{
 
 		//Se crea un manager de overlays. _overlayPlay
+		/*
 		Ogre::OverlayManager& overlayManager = Ogre::OverlayManager::getSingleton();
 		_overlayPlay = overlayManager.create("_overlayPlay");
 
@@ -186,11 +312,11 @@ namespace Logic
         panelPlayers->setMaterialName("hudPlayers");
 
 
-			_textBoxArea[PLAYERS] = static_cast<Ogre::TextAreaOverlayElement*>(
-			overlayManager.createOverlayElement("TextArea", "textAreaPanelPlayers"));
-			_textBoxArea[PLAYERS]->setMetricsMode(Ogre::GMM_PIXELS);
-			_textBoxArea[PLAYERS]->setPosition(45, 10);
-			_textBoxArea[PLAYERS]->setDimensions(40, 20);
+		_textBoxArea[PLAYERS] = static_cast<Ogre::TextAreaOverlayElement*>(
+		overlayManager.createOverlayElement("TextArea", "textAreaPanelPlayers"));
+		_textBoxArea[PLAYERS]->setMetricsMode(Ogre::GMM_PIXELS);
+		_textBoxArea[PLAYERS]->setPosition(45, 10);
+		_textBoxArea[PLAYERS]->setDimensions(40, 20);
 
 
 		std::stringstream sPlayers;//create a stringstream
@@ -245,7 +371,7 @@ namespace Logic
 		_overlayDie->add2D( panelDie );
 
 		_overlayDie->show();
-
+		 */
 		return true;
 
 	} //activate
@@ -295,7 +421,7 @@ namespace Logic
 		_health+= value;
 		std::stringstream sHealth;
 		sHealth << _health;
-		_textBoxArea[HEALTH]->setCaption(sHealth.str());
+		_textBoxArea[HEALTH]->setText(sHealth.str());
 	}
 
 	//short int valor: may be 1 or -1. Depends if a player has dead or another player has enter in the base player.
@@ -303,31 +429,31 @@ namespace Logic
 		_playersInBase+= valor;
 		std::stringstream sPlayers;
 		sPlayers << _playersInBase;
-		_textBoxArea[PLAYERS]->setCaption(sPlayers.str());
+		_textBoxArea[PLAYERS]->setText(sPlayers.str());
 	}
 
 
 	void CHudOverlay::hudSpawn(int spawmTime){
 		
 		if(_overlayPlay->isVisible()){
-			_overlayPlay->hide();
+			_overlayPlay->setVisible(false);
 		}
 
 		std::stringstream sSpawn;
 		sSpawn << "HAS MUERTO, LOSER \n Tiempo de respawn: " << spawmTime;
-		_textAreaDie->setCaption(sSpawn.str());
+		_textAreaDie->setText(sSpawn.str());
 		
 		if(!_overlayDie->isVisible())
-			_overlayDie->show();
+			_overlayDie->setVisible(true);
 		if(spawmTime <= 0.1){
 
-			_overlayDie->hide();
+			_overlayDie->setVisible(false);
 			//reset para volver a mostrar solo el arma inicial al hacer show
 			/*for(int i=1; i<_numWeapons;++i){
 				_weaponsBox[i][NO_WEAPON]->show();
 			}
 			*/
-			_overlayPlay->show();
+			_overlayPlay->setVisible(true);
 		}
 
 	}
@@ -335,9 +461,9 @@ namespace Logic
 
 	void CHudOverlay::hudVisor(){
 		if(_overlayPlay->isVisible())
-			_overlayPlay->hide();
+			_overlayPlay->setVisible(false);
 		else
-			_overlayPlay->show();
+			_overlayPlay->setVisible(true);
 	}
 
 
