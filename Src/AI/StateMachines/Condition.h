b0593c83b22addf096ee77e9cc4c4db1d36c4e66
peplopez <peplopezsanchez@gmail.com>
2013-03-10 21:02:32 +0100

    He creado un proyecto de IA para meter las maquinas de estado ahí.  Lo he dejado como AI al final. Su principal objetivo es que estén ahí las máquinas de estado para combate y para manejar personajes que hagan uso de máquinas de estado.

---
+++ Src/AI/StateMachines/Condition.h
@@ -0,0 +1,201 @@
+/**
+@file CCondition.h
+
+En este fichero se define la interfaz para las condiciones
+de las aristas de las máquinas de estado, así como cuatro 
+condiciones de ejemplo: CConditionTrue (una condición que 
+siempre se evalúa como verdadera), CConditionFinished (es 
+verdadera si la acción asociada al nodo ha terminado), 
+CConditionSuccess (es  verdadera si la acción asociada al 
+nodo ha terminado con éxito) y CConditionFail (es verdadera 
+si la acción asociada al nodo ha terminado con fallo).
+
+@author Gonzalo Flórez
+@date Diciembre 2010
+
+*/
+
+#pragma once
+
+#ifndef __AI_Condition_H
+#define __AI_Condition_H
+
+#include "Logic/Entity/Entity.h"
+#include "../Messages/Message.h"
+
+#include "../LatentActions/LatentAction.h"
+
+using namespace Logic;
+
+namespace Logic 
+{
+	/**
+	Interfaz que deberán implementar las condiciones de 
+	las máquinas de estado.
+	*/
+	template <class TNode>
+	class ICondition
+	{
+	public:
+		/**
+		En este método se comprobará la condición. Recibe el 
+		nodo al que pertenece la condición (de esta manera 
+		podemos comprobar si el nodo ha terminado y en qué 
+		condiciones) y la entidad que ejecuta el comportamiento 
+		(que puede ser NULL).
+
+		@param currentNode Nodo al que pertenece la condición.
+		@param entity Entidad que ejecuta el comportamiento
+		@return true o false según si se cumple la condición o no.
+		*/
+		virtual bool check(TNode* currentNode, CEntity* entity) = 0;
+		/**
+		Devuelve true si a la acción le interesa el tipo de mensaje
+		enviado como parámetro.
+		@param msg Mensaje que ha recibido la entidad.
+		@return true Si la acción está en principio interesada
+		por ese mensaje.
+		*/
+		virtual bool accept(const CMessage *message) {return false;};
+		/**
+		Procesa el mensaje recibido. El método es invocado durante la
+		ejecución de la acción cuando se recibe el mensaje.
+
+		@param msg Mensaje recibido.
+		*/
+		virtual void process(CMessage *message) {};
+	}; // class CCondition
+
+	/**
+	Esta clase define una condición que siempre se evalúa a true.
+	*/
+	template <class TNode>
+	class CConditionTrue : public ICondition<TNode>
+	{
+	public:
+		bool check(TNode* currentNode, CEntity* entity) { return true; }
+	};
+	
+	/**
+	Esta clase define una condición que se evalúa a true si la 
+	acción a la que pertenece ha terminado.
+	*/
+	class CConditionFinished : public ICondition<CLatentAction>
+	{
+	public:
+		bool check(CLatentAction* currentNode, CEntity* entity) 
+		{ 
+			// TODO PRÁCTICA IA
+			// Comprobar si el estado del nodo que se está ejecutando es terminado
+			return currentNode->getStatus() == CLatentAction::SUCCESS ||
+					currentNode->getStatus() == CLatentAction::FAIL;
+		}
+	};
+
+	/**
+	Esta clase define una condición que se evalúa a true si la 
+	acción a la que pertenece ha terminado con éxito.
+	*/
+	class CConditionSuccess : public ICondition<CLatentAction>
+	{
+	public:
+		bool check(CLatentAction* currentNode, CEntity* entity) 
+		{ 
+			// TODO PRÁCTICA IA
+			// Comprobar si el estado del nodo que se está ejecutando es éxito
+
+			return currentNode->getStatus() == CLatentAction::SUCCESS;
+		}
+	};
+
+	/**
+	Esta clase define una condición que se evalúa a true si la 
+	acción a la que pertenece ha terminado con fallo.
+	*/
+	class CConditionFail : public ICondition<CLatentAction>
+	{
+	public:
+		bool check(CLatentAction* currentNode, CEntity* entity) 
+		{ 
+			
+			// TODO PRÁCTICA IA
+			// Comprobar si el estado del nodo que se está ejecutando es fallo
+			return currentNode->getStatus() == CLatentAction::FAIL;
+		}
+	};
+
+	/**
+	Esta clase define una condición que devuelve true 
+	si se recibió un mensaje de un tipo concreto en el último tick
+	*/
+	template <class TNode>
+	class CConditionMessage : public ICondition<TNode>
+	{
+	public:
+		/**
+		En el constructor recibimos el tipo de mensaje que 
+		hará saltar la condición
+		@param messageType Tipo de mensaje que estamos escuchando
+		*/
+		//Message::TMessageType::
+		CConditionMessage(Logic::Message::TMessageType messageType, Logic::Message::TActionType actionType) : _received(false) {
+			_messageType = messageType;
+			_actionType = actionType;
+		}
+		/**
+		En el check sólo tenemos que comprobar el flag _received. Este flag
+		se habrá activado en process si durante este tick hemos recibido
+		un mensaje del tipo adecuado.
+		Una vez hecha la comprobación, reseteamos el flag a false.
+		@param currentNode Nodo al que pertenece la condición (no se usa)
+		@param entity Entidad que ejecuta el comportamiento (no se usa)
+		@return true o false según si se cumple la condición o no.
+
+		*/
+		bool check(TNode* currentNode, CEntity* entity) { 
+			// TODO PRÁCTICA IA
+			// Tenemos que comprobar el flag _received y luego actualizarlo
+			// de nuevo a false para el siguiente tick
+			bool receivedThisTick = _received;
+			_received = false;
+			return receivedThisTick;
+		}
+		/**
+		Devuelve true si el tipo del mensaje recibido coincide con el 
+		que estamos esperando
+		@param msg Mensaje que ha recibido la entidad.
+		@return true Si la acción está en principio interesada
+		por ese mensaje.
+		*/
+		virtual bool accept(const CMessage *message) {
+			// TODO PRÁCTICA IA
+			// La condición debe aceptar mensajes del tipo indicado en _messageType
+			return (message->getType() == _messageType && message->getAction()==_actionType);
+		};
+		/**
+		Procesa el mensaje recibido. Como sólo aceptamos mensajes del
+		tipo en el que estamos interesados aquí directamente ponemos 
+		el flag a true.
+
+		@param msg Mensaje recibido.
+		*/
+		virtual void process(CMessage *message) {
+			// TODO PRÁCTICA IA
+			// Actualizamos el flag _received si el mensaje es del tipo _messageType
+			//CMessageBoolString *maux = static_cast<CMessageBoolString*>(message);
+			_received = (message->getType() == _messageType && message->getAction()==_actionType) || _received;
+		};
+
+	private:
+		/** Flag que se activa cuando recibimos un mensaje del tipo en el que estamos interesados */
+		bool _received;
+		/** Tipo del mensaje que esperamos */
+		Logic::Message::TMessageType _messageType;
+	 	
+		/** Tipo del mensaje que esperamos */
+		Logic::Message::TActionType _actionType;
+	};
+
+} // namespace Logic
+
+#endif // __AI_Condition_H
